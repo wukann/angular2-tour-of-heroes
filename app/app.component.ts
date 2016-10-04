758e98e7f6fcec984ff2c8cc7e47ea322d38f534
wukann <wukann@gmail.com>
2016-10-05 08:05:24 +0900

    refactoring

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
