9d44d1703bb1ccd81a42183c8c215b4452eb3e3f
wukann <wukann@gmail.com>
2016-10-06 23:34:27 +0900

    implement dashboard

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
-    selector : 'my-dashboard',
-    template: '<h3>My Dashboard</h3>'
+    moduleId: module.id,
+    selector: 'my-dashboard',
+    templateUrl: 'dashboard.component.html'
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void { 
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+
+    gotoDetail(hero: Hero): void {}
+}
