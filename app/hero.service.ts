2024dc95287fd374ac40de77d27f3b5d48c7598d
wukann <wukann@gmail.com>
2016-10-07 00:37:22 +0900

    implement router to hero-detail

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,6 +5,7 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
@@ -12,5 +13,10 @@ export class HeroService {
     getHeroesSlowly(): Promise<Hero[]> {
         return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000)) // delay 2 seconds
             .then(() => this.getHeroes());
+    }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
     }
 }
