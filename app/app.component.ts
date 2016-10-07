92f15e05e578455c6b8213930efc26bfccd30777
wukann <wukann@gmail.com>
2016-10-07 22:27:11 +0900

    move AppComponent's template to template file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: `
-      <h1>{{title}}</h1>
-      <nav>
-        <a routerLink="/dashboard">Dashboard</a>
-        <a routerLink="/heroes">Heroes</a>
-      </nav>
-      <router-outlet></router-outlet>
-    `
+    templateUrl: 'app.component.html' 
 })
 export class AppComponent {
     title = 'Tour of Heroes';
