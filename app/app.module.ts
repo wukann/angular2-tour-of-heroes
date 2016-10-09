4a03f8459cd6fb1f21467b7eb136038277c08152
wukann <wukann@gmail.com>
2016-10-09 13:30:08 +0900

    format code

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,26 +2,26 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { AppComponent }   from './app.component';
-import { routing } from './app.routing';
+import { AppComponent }        from './app.component';
+import { routing }             from './app.routing';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroService } from './hero.service';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
-  imports: [ 
-    BrowserModule, 
+  imports: [
+    BrowserModule,
     FormsModule,
-    routing 
+    routing
   ],
-  declarations: [ 
+  declarations: [
     AppComponent,
-    DashboardComponent, 
+    DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent 
+    HeroesComponent
   ],
-  providers: [ HeroService ],
-  bootstrap: [ AppComponent ]
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
