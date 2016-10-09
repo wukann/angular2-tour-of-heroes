df12fcfccb8b5593ad6eef4cdcddd6b0077082bf
wukann <wukann@gmail.com>
2016-10-09 14:46:43 +0900

    replace datastore from mock to in-memory-data

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }        from './app.component';
 import { routing }             from './app.routing';
@@ -13,6 +17,8 @@ import { HeroService }         from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [
