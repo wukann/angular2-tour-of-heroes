700870b8d3ed26ca8643045ac81af57fedee475b
wukann <wukann@gmail.com>
2016-10-09 15:52:40 +0900

    implement update hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -9,6 +9,7 @@ import { Hero } from './hero';
 export class HeroService {
 
     private heroesUrl = 'app/heroes';
+    private headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(private http: Http) {}
 
@@ -29,6 +30,14 @@ export class HeroService {
         return this.http.get(url)
             .toPromise()
             .then(response => response.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http.put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
             .catch(this.handleError);
     }
 
