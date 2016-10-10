6ee56924eeadf6e093a72cf3a9edd01f3f212da6
wukann <wukann@gmail.com>
2016-10-10 09:24:45 +0900

    implement hero search

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,5 @@
+import './rxjs-extensions';
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
@@ -11,6 +13,7 @@ import { routing }             from './app.routing';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { DashboardComponent }  from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component'
 import { HeroService }         from './hero.service';
 
 @NgModule({
@@ -25,7 +28,8 @@ import { HeroService }         from './hero.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
